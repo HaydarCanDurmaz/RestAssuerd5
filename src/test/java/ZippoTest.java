c2f54343570828ef94b6fa15af4b594c37e7525a
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-04-29 16:28:47 +0300

    test1
    checkCounrtryInResponseBody
    
    checkstateInResponseBody
    
    checkHasItemy
    
    bodyArrayHasSizeTest
    
    combiningTest

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -36,6 +36,85 @@ public class ZippoTest {
                 .statusCode(200) // dönüş kodu 200 mü
         ;
     }
+    @Test
+    public void checkCounrtryInResponseBody(){
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("country",equalTo("United States")); // body nin country degişkeni " United States eşitmi
+
+
+        // pm.response.json().id -> body.id
+
+    }
+    //
+//    PM                            RestAssured
+//    body.country                  body("country")
+//    body.'post code'              body("post code")
+//    body.places[0].'place name'   body("places[0].'place name'")
+//    body.places.'place name'      body("places.'place name'")
+//    bütün place nameleri bir arraylist olarak verir
+//    https://jsonpathfinder.com/
+
+    @Test
+    public void checkstateInResponseBody(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places[0].state", equalTo("California"))
+        ;
+    }
+    @Test
+    public void checkHasItemy(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/tr/01000")
+
+                .then()
+               // .log().body()
+                .statusCode(200)
+                .body("places.'place name'", hasItem("Dörtağaç Köyü"))
+        // bütün place name lerin herhangi birinde Dörtağaç Köyü varmı
+        ;
+    }
+    @Test
+    public void bodyArrayHasSizeTest(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places", hasSize(1))
+        ;
+    }
+    @Test
+    public void combiningTest(){
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                //.log().body()
+                .statusCode(200)
+                .body("places", hasSize(1))  // size ı 1 mi
+                .body("places.state", hasItem("California")) // verilen path deki list bu item e sahip mi
+                .body("places[0].'place name'", equalTo("Beverly Hills")) // verilen path deki değer buna eşit mi
+        ;
+    }
+
 
 
 
