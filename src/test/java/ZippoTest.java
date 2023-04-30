8d67ae24cd2b3c2120d024a4bd46d1cb7daa5b52
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-04-30 11:43:42 +0300

    ResponseSpecificatio  - RequestSpecification  - requestResponseSpecificationn -     extractingJsonPath     -extractingJsonPath2     - extractingJsonPath3     - extractingJsonPath4      -extractingJsonPath5      - extractingJsonPathResponsAll

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,4 +1,5 @@
 
+
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
@@ -10,49 +11,75 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.xml.stream.Location;
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-
 public class ZippoTest {
+
     @Test
     public void test() {
-        given()          // hazırlık işlemleri  : (token,send body, parametreler)
-                .when()  // endpoint (url), metodu
-                .then(); // assertion, test, data işlemleri
+
+        given()
+                // Hazırlık işlemleri : (token,send body, parametreler)
+
+                .when()
+                // endpoint (url), metodu
+
+                .then()
+        // assertion, test, data işlemleri
+        ;
     }
 
     @Test
-    public void test1() {
-
+    public void statusCodeTest() {
 
         given()
 
                 .when()
                 .get("http://api.zippopotam.us/us/90210")
+
                 .then()
                 .log().body()    // dönen body json datat sı,   log.all()
                 .statusCode(200) // dönüş kodu 200 mü
         ;
     }
 
+
     @Test
-    public void checkCounrtryInResponseBody() {
+    public void contentTypeTest() {
+
         given()
+
                 .when()
                 .get("http://api.zippopotam.us/us/90210")
-                .then()
-                .log().body()
-                .statusCode(200)
-                .body("country", equalTo("United States")); // body nin country degişkeni " United States eşitmi
 
+                .then()
+                .log().body()    // dönen body json datat sı,   log.all()
+                .statusCode(200) // dönüş kodu 200 mü
+                .contentType(ContentType.JSON) // dönen sonuç JSON mı
+        ;
+    }
+
+    @Test
+    public void checkCountryInResponseBody() {
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()    // dönen body json datat sı,   log.all()
+                .statusCode(200) // dönüş kodu 200 mü
+                .body("country", equalTo("United States")) // body nin country değişkeni "United States" eşit Mİ
 
         // pm.response.json().id -> body.id
-
+        ;
     }
-    //
+//
 //    PM                            RestAssured
 //    body.country                  body("country")
 //    body.'post code'              body("post code")
@@ -83,7 +110,7 @@ public class ZippoTest {
                 .get("http://api.zippopotam.us/tr/01000")
 
                 .then()
-                // .log().body()
+                //.log().body()
                 .statusCode(200)
                 .body("places.'place name'", hasItem("Dörtağaç Köyü"))
         // bütün place name lerin herhangi birinde Dörtağaç Köyü varmı
@@ -120,28 +147,25 @@ public class ZippoTest {
         ;
     }
 
+
     @Test
     public void pathParamTest() {
-
-
         given()
                 .pathParam("ulke", "us")
-                .pathParam("pstakodu", 90210)
+                .pathParam("postaKod", 90210)
                 .log().uri() // request Link
 
                 .when()
-                .get("http://api.zippopotam.us/{ulke}/{pstakodu}")
+                .get("http://api.zippopotam.us/{ulke}/{postaKod}")
 
                 .then()
                 .statusCode(200)
         //.log().body()
         ;
-
     }
 
     @Test
     public void queryParamTest() {
-
         // https://gorest.co.in/public/v1/users?page=3
 
         given()
@@ -155,8 +179,6 @@ public class ZippoTest {
                 .statusCode(200)
                 .log().body()
         ;
-
-
     }
 
     @Test
@@ -164,7 +186,6 @@ public class ZippoTest {
         // https://gorest.co.in/public/v1/users?page=3
         // bu linkteki 1 den 10 kadar sayfaları çağırdığınızda response daki donen page degerlerinin
         // çağrılan page nosu ile aynı olup olmadığını kontrol ediniz.
-
 
         for (int i = 1; i < 10; i++) {
             given()
@@ -182,5 +203,160 @@ public class ZippoTest {
         }
 
     }
-}
 
+
+    RequestSpecification requestSpec;
+    ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public void Setup() {
+
+        baseURI = "https://gorest.co.in/public/v1";
+
+        requestSpec = new RequestSpecBuilder()
+                .log(LogDetail.URI)
+                .setContentType(ContentType.JSON)
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .log(LogDetail.BODY)
+                .build();
+    }
+
+
+    @Test
+    public void requestResponseSpecificationn() {
+        // https://gorest.co.in/public/v1/users?page=3
+
+        given()
+                .param("page", 1)  // ?page=1  şeklinde linke ekleniyor
+                .spec(requestSpec)
+
+                .when()
+                .get("/users")  // ?page=1
+
+                .then()
+                .spec(responseSpec)
+        ;
+    }
+
+    @Test
+    public void extractingJsonPath() {
+
+        String countryName =
+                given()
+                        .when()
+                        .get("http://api.zippopotam.us/us/90210")
+
+                        .then()
+                        //.log().body()
+                        .extract().path("country");
+
+        System.out.println("countryName = " + countryName);
+        Assert.assertEquals(countryName, "United States");
+    }
+
+    @Test
+    public void extractingJsonPath2() {
+        //placeName
+        String placeName =
+                given()
+                        .when()
+                        .get("http://api.zippopotam.us/us/90210")
+
+                        .then()
+                        .log().body()
+                        .extract().path("places[0].'place name'")  // places[0]['place name']
+                ;
+
+        System.out.println("placeName = " + placeName);
+        Assert.assertEquals(placeName, "Beverly Hills");
+    }
+
+    @Test
+    public void extractingJsonPath3() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki limit bilgisini yazdırınız.
+
+        int limit =
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().path("meta.pagination.limit");
+
+        System.out.println("limit = " + limit);
+    }
+
+    @Test
+    public void extractingJsonPath4() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki bütün idleri yazdırınız.
+
+        List<Integer> idler =
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .statusCode(200)
+                        .extract().path("data.id"); // bütün id leri ver
+        ;
+
+        System.out.println("idler = " + idler);
+    }
+
+    @Test
+    public void extractingJsonPath5() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki bütün name lei yazdırınız.
+
+        List<String> names =
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .statusCode(200)
+                        .extract().path("data.name"); // bütün id leri ver
+        ;
+
+        System.out.println("names = " + names);
+    }
+
+    @Test
+    public void extractingJsonPathResponsAll() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki bütün name lei yazdırınız.
+
+        Response donenData =
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .statusCode(200)
+                        // .log().body()
+                        .extract().response(); // dönen tüm datayı verir.
+        ;
+
+        List<Integer> idler = donenData.path("data.id");
+        List<String> names = donenData.path("data.name");
+        int limit = donenData.path("meta.pagination.limit");
+
+        System.out.println("idler = " + idler);
+        System.out.println("names = " + names);
+        System.out.println("limit = " + limit);
+
+        Assert.assertTrue(names.contains("Dakshayani Pandey"));
+        Assert.assertTrue(idler.contains(1203767));
+        Assert.assertEquals(limit, 10, "test sonucu hatalı");
+    }
+
+
+}
