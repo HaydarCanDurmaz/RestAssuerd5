ad126317195bb8d470e98ef637433230447c1e18
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-05-05 10:59:53 +0300

    deleteUser deleteUserNegative
    extractingPath
    extractingJsonPath

---
+++ src/test/java/PathAndJsonPath.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PathAndJsonPath {
+
+    @Test
+    public void extractingPath() {
+// "post code": "90210"
+        int postCode =
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .extract().path("'post code'")
+        ;
+        System.out.println("postCode = " + postCode);
+    }
+    @Test
+    public void extractingJsonPath() {
+// "post code": "90210"
+        int postCode =
+                given()
+                        .when()
+                        .get("http://api.zippopotam.us/us/90210")
+
+                        .then()
+                        .log().body()
+                        .extract().jsonPath().getInt("'post code'")
+                ;
+        System.out.println("postCode = " + postCode);
+    }
+}
